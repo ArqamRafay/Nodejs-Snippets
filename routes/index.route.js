88c51adef5734af25cacd287e3dc6a87d19da877
Arqam <Arqam.rafay@sixlogs.com>
2023-03-16 17:59:21 +0500

    Add example of worker thread

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,6 +3,8 @@ let express = require('express'),
 
 
 router.use('/cache', require('./cache.route'));
+router.use('/workerThread', require('./workerThread.route'));
+
 router.get('*', (req, res) => {
     res.status(200).send({
         message: 'Welcome to the beginning of nothingness.',
